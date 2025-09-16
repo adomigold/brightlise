d44207ace6f695bf39ac8d30b75335ccdc1a6844
adolph gasper <adolphgasper@gmail.com>
2025-09-16 14:05:14 +0300

    add seo

---
+++ next-sitemap.js
@@ -0,0 +1,4 @@
+module.exports = {
+  siteUrl: "https://www.liseschools.ac.tz",
+  generateRobotsTxt: true,
+};
--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -11,16 +11,56 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Required meta tags */}
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <title>Lise Schools Tanzania | Quality Education</title>
+        <meta name="description" content="Lise Schools in Tanzania provides world-class education with a focus on academic excellence, innovation, and growth." />
+
+        {/* Open Graph for Facebook/LinkedIn */}
+        <meta property="og:title" content="Lise Schools Tanzania" />
+        <meta property="og:description" content="World-class education in Tanzania." />
+        <meta property="og:image" content="https://www.liseschools.ac.tz/og-image.jpg" />
+        <meta property="og:url" content="https://www.liseschools.ac.tz/" />
+        <meta property="og:type" content="website" />
+
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Lise Schools Tanzania" />
+        <meta name="twitter:description" content="World-class education in Tanzania." />
+        <meta name="twitter:image" content="https://www.liseschools.ac.tz/og-image.jpg" />
+
+        {/* Canonical URL */}
+        <link rel="canonical" href="https://www.liseschools.ac.tz/" />
 
         {/* Favicon icon */}
         <link rel="shortcut icon" type="image/x-icon" href="/img/logo/ico.jpg" />
-        <title>Brigth Lise - Pre and Primary School</title>
+
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "School",
+              "name": "Lise Schools Tanzania",
+              "url": "https://www.liseschools.ac.tz/",
+              "logo": "https://www.liseschools.ac.tz/img/logo/ico.jpg",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "Kibaha Bokotimiza, Pwani",
+                "addressLocality": "Kibaha",
+                "addressCountry": "TZ"
+              },
+              "contactPoint": {
+                "@type": "ContactPoint",
+                "telephone": "+255713785875",
+                "contactType": "Admissions"
+              }
+            }),
+          }}
+        />
+
       </Head>
 
       <Component {...pageProps} />
+      
     </>
   )
 }
